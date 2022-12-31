162b3690190eb33cadf7b3c08fb4d34706b9def1
AdrienMasanet <adrien.masanet@hotmail.fr>
2022-12-31 13:38:29 +0100

    feat: section component

---
+++ components/Section/Section.tsx
@@ -0,0 +1,29 @@
+import styles from "./Section.module.scss";
+import Waves, { WavesDirection } from "../Waves/Waves";
+
+type SectionProps = {
+  title: string;
+  sectionColor: string;
+  nextSectionColor: string;
+  textColor: string;
+  children: React.ReactNode;
+  waveSection?: boolean;
+};
+
+const Section = ({ title, sectionColor, nextSectionColor, textColor, children, waveSection }: SectionProps) => {
+  return (
+    <>
+      {waveSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={sectionColor} wavesSpeed={4} absolute={true} />}
+      <section className={styles.section} style={{ backgroundColor: sectionColor, color: textColor }}>
+        <div className="maxed-container">
+          <h2 className={styles.title}>{title}</h2>
+          {children}
+          <hr className={waveSection ? styles.spacersm : styles.spacerlg} />
+        </div>
+        {waveSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={nextSectionColor} wavesSpeed={4} />}
+      </section>
+    </>
+  );
+};
+
+export default Section;
