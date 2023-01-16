063a3cc19bfb9baa383a9b6389c2e102013f2b38
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-16 22:37:26 +0100

    fix: section component top and bottom spacers as props

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,27 +1,40 @@
 import styles from "./Section.module.scss";
 import Waves from "../Waves/Waves";
-import { WavesDirection } from "../Waves/types";
 
 type SectionProps = {
   title: string;
+  subtitle?: string;
   sectionColor: string;
   nextSectionColor: string;
   textColor: string;
   children: React.ReactNode;
   waveSection?: boolean;
+  lastSection?: boolean;
+  spaceBelow?: string;
+  spaceAfterTitles?: string;
 };
 
-const Section = ({ title, sectionColor, nextSectionColor, textColor, children, waveSection }: SectionProps) => {
+const Section = ({ title, subtitle, sectionColor, nextSectionColor, textColor, children, waveSection, lastSection, spaceBelow = "", spaceAfterTitles = "" }: SectionProps) => {
+  let spacerBelowSize: string = spaceBelow.toString();
+  let spacerAfterTitlesSize: string = spaceAfterTitles.toString();
+
+  if (spacerBelowSize == "") {
+    if (!waveSection) spacerBelowSize = "spacer-lg";
+    else spacerBelowSize = "spacer-md";
+  }
+
   return (
     <>
       {waveSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={sectionColor} wavesSpeed={4} absolute={true} />}
       <section className={styles.section} style={{ backgroundColor: sectionColor, color: textColor }}>
         <div className="maxed-container">
           <h2 className={styles.title}>{title}</h2>
+          {subtitle && <h3 className={styles.subtitle}>{subtitle}</h3>}
+          {spacerAfterTitlesSize != "" && <hr className={spacerAfterTitlesSize} />}
           {children}
-          <hr className={waveSection ? styles.spacersm : styles.spacerlg} />
+          {spacerBelowSize != "" && <hr className={spacerBelowSize} />}
         </div>
-        {waveSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={nextSectionColor} wavesSpeed={4} />}
+        {waveSection && !lastSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={nextSectionColor} wavesSpeed={4} />}
       </section>
     </>
   );
