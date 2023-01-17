06fe03a19319250efbe834b7effb5a3964776d24
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-18 00:05:19 +0100

    feat: added maxwidth prop to section component

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -12,9 +12,10 @@ type SectionProps = {
   lastSection?: boolean;
   spaceBelow?: string;
   spaceAfterTitles?: string;
+  maxWidth?: number;
 };
 
-const Section = ({ title, subtitle, sectionColor, nextSectionColor, textColor, children, waveSection, lastSection, spaceBelow = "", spaceAfterTitles = "" }: SectionProps) => {
+const Section = ({ title, subtitle, sectionColor, nextSectionColor, textColor, children, waveSection, lastSection, spaceBelow = "", spaceAfterTitles = "", maxWidth }: SectionProps) => {
   let spacerBelowSize: string = spaceBelow.toString();
   let spacerAfterTitlesSize: string = spaceAfterTitles.toString();
 
@@ -29,9 +30,15 @@ const Section = ({ title, subtitle, sectionColor, nextSectionColor, textColor, c
       <section className={styles.section} style={{ backgroundColor: sectionColor, color: textColor }}>
         <div className="maxed-container">
           <h2 className={styles.title}>{title}</h2>
+
           {subtitle && <h3 className={styles.subtitle}>{subtitle}</h3>}
+
           {spacerAfterTitlesSize != "" && <hr className={spacerAfterTitlesSize} />}
-          {children}
+
+          <div className={styles.children} style={{ maxWidth: maxWidth ? maxWidth + "px" : "", margin: "auto" }}>
+            {children}
+          </div>
+
           {spacerBelowSize != "" && <hr className={spacerBelowSize} />}
         </div>
         {waveSection && !lastSection && <Waves wavesNumber={3} wavesSmoothing={350} wavesColor={nextSectionColor} wavesSpeed={4} />}
