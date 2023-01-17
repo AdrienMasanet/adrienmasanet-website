aa3f3727e109e676028867f6279557e3c0b1fc18
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-18 00:08:18 +0100

    feat: personalitytrait component

---
+++ components/PersonalityTrait/PersonalityTrait.tsx
@@ -0,0 +1,28 @@
+import styles from "./PersonalityTrait.module.scss";
+import { useInView } from "react-intersection-observer";
+import Image from "next/image";
+
+type PersonalityTraitProps = {
+  name: string;
+  description: string;
+  image: string;
+};
+
+const PersonalityTrait = ({ name, description, image }: PersonalityTraitProps) => {
+  const [imageRef, isImageInView, entry] = useInView({
+    threshold: 0.5,
+    rootMargin: "-5.5% 0px -5.5% 0px",
+  });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.nameanddescriptioncontainer}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.description}>{description}</p>
+      </div>
+      <Image className={`${styles.image} ${isImageInView ? styles.active : ""}`} ref={imageRef} src={image} alt={name} width={160} height={160} />
+    </div>
+  );
+};
+
+export default PersonalityTrait;
