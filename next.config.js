ddd8aae9e653cea8d2ae6f52673201e7d6c71afa
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-23 21:23:05 +0100

    feat: allow images from backend pocketbase api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [process.env.POCKETBASE_API_DOMAIN],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
