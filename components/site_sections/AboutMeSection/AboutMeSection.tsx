48f7f8cc9da5a695d2e3d98cba2a857d5f8f517e
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-25 23:50:55 +0100

    feat: aboutmesection component

---
+++ components/site_sections/AboutMeSection/AboutMeSection.tsx
@@ -0,0 +1,21 @@
+import styles from "./AboutMeSection.module.scss";
+import scssThemeVariables from "../../../styles/javascript_variables.module.scss";
+import Section from "../../Section/Section";
+import PersonalityTrait from "../../PersonalityTrait/PersonalityTrait";
+
+type AboutMeSectionProps = {
+  personalityTraits: any;
+};
+
+const AboutMeSection = ({ personalityTraits }: AboutMeSectionProps) => {
+  return (
+    <Section waveSection={true} title="Mes traits de personnalité" subtitle="Mais qui est donc cet énergumène ?" sectionColor={scssThemeVariables.scssThemeClWhite} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClDarkblue} spaceAfterTitles="spacer-md" maxWidth={900}>
+      {personalityTraits &&
+        personalityTraits.map((trait: any) => {
+          return <PersonalityTrait key={trait.id} name={trait.name} description={trait.description} image={trait.image} />;
+        })}
+    </Section>
+  );
+};
+
+export default AboutMeSection;
