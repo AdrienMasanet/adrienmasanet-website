3fbdb99f368dc5f529a567d88b9ed0a842a93e68
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-25 23:52:49 +0100

    feat: import and use section components on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import HeaderSection from "../components/site_sections/HeaderSection/HeaderSection";
+import SkillsSection from "../components/site_sections/SkillsSection/SkillsSection";
+import TechsSection from "../components/site_sections/TechsSection/TechsSection";
+import AboutMeSection from "../components/site_sections/AboutMeSection/AboutMeSection";
+import FooterSection from "../components/site_sections/FooterSection/FooterSection";
+import HobbiesSection from "../components/site_sections/HobbiesSection/HobbiesSection";
+import ContactSection from "../components/site_sections/ContactSection/ContactSection";
 import { fetchSkills, fetchTechs, fetchPersonalityTraits, fetchHobbies } from "../services/apiRequests";
 
 export default async function MainPage() {
@@ -8,7 +15,15 @@ export default async function MainPage() {
 
   return (
     <>
-      <div className="main-container"></div>
+      <div className="main-container">
+        <HeaderSection />
+        <SkillsSection skills={skills} />
+        <TechsSection masteredCategoriesAndElements={masteredTechs} notMasteredCategoriesAndElements={masteredTechs} />
+        <AboutMeSection personalityTraits={personalityTraits} />
+        <HobbiesSection hobbies={hobbies} />
+        <ContactSection />
+        <FooterSection />
+      </div>
     </>
   );
 }
