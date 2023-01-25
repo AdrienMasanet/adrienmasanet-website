c6981b0031acfa7fa61e752dffb61e33a882ae96
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-01-25 23:49:57 +0100

    feat: headersection component

---
+++ components/site_sections/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,23 @@
+import styles from "./HeaderSection.module.scss";
+
+const HeaderSection = () => {
+  return (
+    <header className={styles.background}>
+      <div className={styles.overlay}>
+        <div className={`${styles.portraitandtitlescontainer} maxed-container`}>
+          <div className={styles.portrait}></div>
+          <div className={styles.titlescontainer}>
+            <h1 className={styles.title}>
+              Adrien
+              <br />
+              Masanet
+            </h1>
+            <h2 className={styles.subtitle}>Développeur polyvalent passionné</h2>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderSection;
