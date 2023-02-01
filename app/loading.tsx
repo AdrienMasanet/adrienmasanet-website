da8eb4f17038bf9d717b7b50b407f6a682cb91f8
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-01 02:01:09 +0100

    feat: loading page minimalist design

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
+import scssThemeVariables from "../styles/javascript_variables.module.scss";
+
 export default function LoadingMainPage() {
-  return <h1>Loading</h1>;
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: scssThemeVariables.scssThemeClWhite, display: "flex" }}>
+      <div style={{ color: scssThemeVariables.scssThemeClDarkblue, margin: "auto", fontSize: "3em" }}>Chargement</div>
+    </div>
+  );
 }
