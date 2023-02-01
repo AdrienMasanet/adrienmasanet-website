a1d90588aa0ba48762b1479e0da08002a3708332
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-01 02:01:03 +0100

    feat: error page minimalist design

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,11 @@
 "use client";
 
-export default function MainPageError() {
-  return <h1>Error</h1>;
+import scssThemeVariables from "../styles/javascript_variables.module.scss";
+
+export default function LoadingMainPage() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", background: scssThemeVariables.scssThemeClWhite, display: "flex" }}>
+      <div style={{ color: scssThemeVariables.scssThemeClDarkblue, margin: "auto", fontSize: "3em" }}>Erreur</div>
+    </div>
+  );
 }
