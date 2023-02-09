07352f514a5067e217bcfc8f1bc53cd34cc48d1b
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-09 23:45:38 +0100

    fix: changed wrong props object name

--- components/site_sections/TechsSection/TechsSection.tsx
+++ components/site_sections/TechsSection/TechsSection.tsx
@@ -4,18 +4,18 @@ import Section from "../../Section/Section";
 import ElementsShowcase from "../../ElementsShowcase/ElementsShowcase";
 import { ElementsShowcaseCategory } from "../../ElementsShowcase/types";
 
-type SkillsSectionProps = {
+type TechsSectionProps = {
   masteredCategoriesAndElements: ElementsShowcaseCategory[];
   notMasteredCategoriesAndElements: ElementsShowcaseCategory[];
 };
 
-const TechsSection = ({ masteredCategoriesAndElements, notMasteredCategoriesAndElements }: SkillsSectionProps) => {
+const TechsSection = ({ masteredCategoriesAndElements, notMasteredCategoriesAndElements }: TechsSectionProps) => {
   return (
     <>
       <Section title="Les technologies que j'aime" subtitle="Et avec lesquelles j’ai de l’expérience" sectionColor={scssThemeVariables.scssThemeClDarkblue} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClWhite} spaceBelow={"spacer-md"} spaceAfterTitles="spacer-sm">
         <ElementsShowcase categoriesAndElements={masteredCategoriesAndElements} />
       </Section>
-      <Section title="Celles qui me font de l’œil" subtitle="Et que j’aimerais pratiquer d’avantage un jour" sectionColor={scssThemeVariables.scssThemeClDarkblue} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClWhite} spaceAfterTitles="spacer-sm">
+      <Section title="Celles qui me font de l’œil" subtitle="Ou que j’aimerais pratiquer d’avantage" sectionColor={scssThemeVariables.scssThemeClDarkblue} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClWhite} spaceAfterTitles="spacer-sm">
         <ElementsShowcase categoriesAndElements={notMasteredCategoriesAndElements} />
       </Section>
     </>
