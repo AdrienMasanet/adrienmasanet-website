1ceeb0668dc37bbb08b2df262ed4f3306e36aa5c
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-10 01:05:11 +0100

    fix: dynamic imports for some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,26 @@
+import dynamic from "next/dynamic";
 import ClientDelayedLoader from "../components/ClientDelayedLoader/ClientDelayedLoader";
 import HeaderSection from "../components/site_sections/HeaderSection/HeaderSection";
 import SkillsSection from "../components/site_sections/SkillsSection/SkillsSection";
 import TechsSection from "../components/site_sections/TechsSection/TechsSection";
+import LanguagesSection from "../components/site_sections/LanguagesSection/LanguagesSection";
+import AchievementsSection from "../components/site_sections/AchievementsSection/AchievementsSection";
 import AboutMeSection from "../components/site_sections/AboutMeSection/AboutMeSection";
 import FooterSection from "../components/site_sections/FooterSection/FooterSection";
-import HobbiesSection from "../components/site_sections/HobbiesSection/HobbiesSection";
-import ContactSection from "../components/site_sections/ContactSection/ContactSection";
-import { fetchSkills, fetchMasteredTechs, fetchNotMasteredTechs, fetchPersonalityTraits, fetchHobbies } from "../services/apiRequests";
+import { fetchSkills, fetchMasteredTechs, fetchNotMasteredTechs, fetchLanguages, fetchPersonalityTraits, fetchHobbies } from "../services/apiRequests";
 
 export default async function MainPage() {
+  // Fetch data from the API for the sections containing data
   const skills = await fetchSkills();
   const masteredTechs = await fetchMasteredTechs();
   const notMasteredTechs = await fetchNotMasteredTechs();
+  const languages = await fetchLanguages();
   const personalityTraits = await fetchPersonalityTraits();
   const hobbies = await fetchHobbies();
+
+  // Dynamic imports for sections that are not needed on the server side
+  const HobbiesSection = dynamic(() => import("../components/site_sections/HobbiesSection/HobbiesSection"), { ssr: false });
+  const ContactSection = dynamic(() => import("../components/site_sections/ContactSection/ContactSection"), { ssr: false });
 
   return (
     <>
@@ -22,6 +29,8 @@ export default async function MainPage() {
         <HeaderSection />
         <SkillsSection skills={skills} />
         <TechsSection masteredCategoriesAndElements={masteredTechs} notMasteredCategoriesAndElements={notMasteredTechs} />
+        <LanguagesSection languages={languages} />
+        <AchievementsSection />
         <AboutMeSection personalityTraits={personalityTraits} />
         <HobbiesSection hobbies={hobbies} />
         <ContactSection />
