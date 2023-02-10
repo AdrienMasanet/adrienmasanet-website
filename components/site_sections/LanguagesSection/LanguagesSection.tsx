2afa5173e7fc550a0803fc4dfd3662a6246341bc
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-10 01:16:02 +0100

    fix: removed log that was used for debugging

--- components/site_sections/LanguagesSection/LanguagesSection.tsx
+++ components/site_sections/LanguagesSection/LanguagesSection.tsx
@@ -4,17 +4,12 @@ import styles from "./LanguagesSection.module.scss";
 import scssThemeVariables from "../../../styles/javascript_variables.module.scss";
 import Section from "../../Section/Section";
 import LanguageItem from "./LanguageItem";
-import { useEffect } from "react";
 
 type LanguagesSectionProps = {
   languages: any;
 };
 
 const LanguagesSection = ({ languages }: LanguagesSectionProps) => {
-  useEffect(() => {
-    console.log(languages);
-  }, [languages]);
-
   return (
     <Section waveSection={true} title="Langues pratiquées" subtitle="Que je puedo hablar and write" sectionColor={scssThemeVariables.scssThemeClWhite} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClDarkblue} spaceAfterTitles="spacer-md" maxWidth={900}>
       <div className={styles.container}>
