7f2fcd84842f9883f6d444f37bc17bef1235b348
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-21 00:21:46 +0100

    feat: achievementssection component

---
+++ components/site_sections/AchievementsSection/AchievementsSection.tsx
@@ -0,0 +1,17 @@
+import styles from "./AchievementsSection.module.scss";
+import scssThemeVariables from "../../../styles/javascript_variables.module.scss";
+import Section from "../../Section/Section";
+
+type AchievementsSectionProps = {};
+
+const AchievementsSection = ({}: AchievementsSectionProps) => {
+  return (
+    <>
+      <Section title="Quelques réalisations" subtitle="Diverses et variées" sectionColor={scssThemeVariables.scssThemeClDarkblue} nextSectionColor={scssThemeVariables.scssThemeClDarkblue} textColor={scssThemeVariables.scssThemeClWhite} spaceBelow={"spacer-md"} spaceAfterTitles="spacer-md" maxWidth={900}>
+        <h4>Réalisations</h4>
+      </Section>
+    </>
+  );
+};
+
+export default AchievementsSection;
