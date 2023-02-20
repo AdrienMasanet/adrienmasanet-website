32491ee231a328bb1caaccc0e76f35ccfff8e833
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-20 23:56:07 +0100

    feat: added codingame and stackoverflow socials

--- components/site_sections/ContactSection/ContactSection.tsx
+++ components/site_sections/ContactSection/ContactSection.tsx
@@ -11,6 +11,8 @@ const ContactSection = () => {
       <div className={styles.socialcontainer}>
         <SocialMediaIcon name={"LinkedIn"} icon={"/images/linkedin.png"} link={"https://www.linkedin.com/in/adrienmasanet"} />
         <SocialMediaIcon name={"Github"} icon={"/images/github.png"} link={"https://github.com/AdrienMasanet"} />
+        <SocialMediaIcon name={"Codingame"} icon={"/images/codingame.png"} link={"https://www.codingame.com/profile/ef9dcf317935f78353d3e35b25c84c733938225"} />
+        <SocialMediaIcon name={"Stackoverflow"} icon={"/images/stackoverflow.png"} link={"https://stackoverflow.com/users/14763037/maza"} />
       </div>
     </Section>
   );
