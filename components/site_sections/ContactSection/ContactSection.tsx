3a4975e2abd66c810420da47f9b0007f5a071e57
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-02-27 21:57:02 +0100

    feat: added welovedevs social

--- components/site_sections/ContactSection/ContactSection.tsx
+++ components/site_sections/ContactSection/ContactSection.tsx
@@ -13,6 +13,7 @@ const ContactSection = () => {
       <div className={styles.socialcontainer}>
         <SocialMediaIcon name={"LinkedIn"} icon={"/images/linkedin.png"} link={"https://www.linkedin.com/in/adrienmasanet"} />
         <SocialMediaIcon name={"Github"} icon={"/images/github.png"} link={"https://github.com/AdrienMasanet"} />
+        <SocialMediaIcon name={"WeLoveDevs"} icon={"/images/welovedevs.png"} link={"https://adrien-masanet.welovedevs.com"} />
         <SocialMediaIcon name={"Codingame"} icon={"/images/codingame.png"} link={"https://www.codingame.com/profile/ef9dcf317935f78353d3e35b25c84c733938225"} />
         <SocialMediaIcon name={"Stackoverflow"} icon={"/images/stackoverflow.png"} link={"https://stackoverflow.com/users/14763037/maza"} />
       </div>
