ddb9c1024b13b70189050328ab8f1cfe5fd3f447
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-09-29 02:09:03 +0200

    fix: remove the ClientDelayedLoader after transition is complete

--- components/ClientDelayedLoader/ClientDelayedLoader.tsx
+++ components/ClientDelayedLoader/ClientDelayedLoader.tsx
@@ -1,15 +1,38 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 import { useLoadingComplete } from "../../hooks/useLoadingComplete";
 import styles from "./ClientDelayedLoader.module.scss";
 
-const ClientDelayedLoader = () => {
+type ClientDelayedLoaderProps = {
+  transitionDelay?: number;
+};
+
+const ClientDelayedLoader = ({
+  transitionDelay = 2000,
+}: ClientDelayedLoaderProps) => {
   const { loadingComplete } = useLoadingComplete();
+  const mainContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (loadingComplete) mainContainerRef.current?.remove();
+    }, transitionDelay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [loadingComplete, transitionDelay]);
 
   return (
     <div
+      ref={mainContainerRef}
       className={styles.container}
-      style={{ opacity: loadingComplete ? "0" : "1" }}
+      style={{
+        transition: `opacity ${transitionDelay}ms ease-in-out`,
+        opacity: loadingComplete ? "0" : "1",
+      }}
     ></div>
   );
 };
