87af2fc883114ebe8e3fea749638668cb86f14fc
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-10-20 02:05:03 +0200

    test: add tests for PersonnalityTrait

---
+++ components/PersonalityTrait/PersonnalityTrait.spec.tsx
@@ -0,0 +1,46 @@
+import "@/testing/__mocks__/intersectionObserverMock";
+
+import { render, screen } from "@testing-library/react";
+
+import PersonalityTrait from "./PersonalityTrait";
+
+describe("PersonnalityTrait component", () => {
+  it("should render the passed name", () => {
+    render(
+      <PersonalityTrait
+        name="test name"
+        description="test description"
+        image="http://test-image"
+      />
+    );
+
+    expect(screen.getByText("test name")).toBeInTheDocument();
+  });
+
+  it("should render the passed description", () => {
+    render(
+      <PersonalityTrait
+        name="test name"
+        description="test description"
+        image="http://test-image"
+      />
+    );
+
+    expect(screen.getByText("test description")).toBeInTheDocument();
+  });
+
+  it("should render the passed image", () => {
+    render(
+      <PersonalityTrait
+        name="test name"
+        description="test description"
+        image="http://test-image"
+      />
+    );
+
+    const image: HTMLImageElement = screen.getByAltText("test name");
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain("test-image");
+  });
+});
