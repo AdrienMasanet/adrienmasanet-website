638ae1e543142418b1e0415c4b7b057cab30c3d1
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-11-08 22:56:34 +0100

    feat: add useMIMEType hook

---
+++ hooks/useMIMEType.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const useMIMEType = (mediaURI: string) => {
+  const [mediaCategory, setMediaCategory] = useState<string>("other");
+  const [mediaExtension, setMediaExtension] = useState<string>("other");
+
+  useEffect(() => {
+    if (!mediaURI) return;
+    const extensionMatch = mediaURI.match(
+      /\.(jpg|jpeg|png|gif|webp|svg|mp4|webm|mp3|wav|flv|avi)$/
+    );
+    if (!extensionMatch) return;
+
+    setMediaExtension(extensionMatch[0]);
+
+    switch (mediaExtension) {
+      case ".jpg":
+      case ".jpeg":
+      case ".png":
+      case ".gif":
+      case ".webp":
+      case ".svg":
+        setMediaCategory("image");
+        break;
+      case ".mp4":
+      case ".webm":
+        setMediaCategory("video");
+        break;
+      case ".mp3":
+      case ".wav":
+        setMediaCategory("audio");
+        break;
+      case ".flv":
+      case ".avi":
+        setMediaCategory("video");
+        break;
+    }
+  }, [mediaURI, mediaExtension]);
+
+  return { mediaCategory, mediaExtension };
+};
+
+export default useMIMEType;
